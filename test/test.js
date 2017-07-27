c5bebc2ff9289ed37e0b89fcc3d3943192a706d5
Emma (Riley) Booker <RileyIO@users.noreply.github.com>
2017-07-26 22:11:33 -0300

    Fixing test error due to local testing

--- test/test.js
+++ test/test.js
@@ -9,7 +9,8 @@ describe('Initialize electron-stats, (Actual) [FromModule]', () => {
   const es = new ElectronStats({
     host: 'localhost', port: 8080,
     application_id: '05cf11ee-c58e-45a8-8092-3efb91ca5d04',
-    authentication: '49c33448343f63abc8bffd6100fe938c07c4e55314a86a777489412092a3f9d0e4455fee2e29fef813242fe3a2794896a2e6f707cd5d811ffeece2f677b62be7'
+    authentication: '49c33448343f63abc8bffd6100fe938c07c4e55314a86a777489412092a3f9d0e4455fee2e29fef813242fe3a2794896a2e6f707cd5d811ffeece2f677b62be7',
+    autoSend: false
   })
   // Test server
   const server = http.createServer((req, res) => {
